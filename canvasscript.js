66f12b68dc29ac9a691c3f2b16656c416e468c66
KumarSaurabhThakur <160490638+KumarSaurabhThakur@users.noreply.github.com>
2025-04-23 16:17:50 +0530

    Update canvasscript.js

--- canvasscript.js
+++ canvasscript.js
@@ -186,8 +186,7 @@ for (i = 0; i <= 10; i++)
 
 
 //PAINT BRUSH PROGRAM
-//PAINT BRUSH PROGRAM
-const canvas = document.querySelector("#canvas");
+/*const canvas = document.querySelector("#canvas");
 
 canvas.width = 900;
 canvas.height = 500;
@@ -200,43 +199,311 @@ let blueBtn = document.querySelector("#blueBtn");
 let greenBtn = document.querySelector("#greenBtn");
 let drawBtn = document.querySelector("#drawBtn");
 
-let brushColor; 
-
 redBtn.addEventListener("click", redColor);
 blueBtn.addEventListener("click", blueColor);
 greenBtn.addEventListener("click", greenColor);
 drawBtn.addEventListener("click", drawClick);
 
 function redColor() {
-    brushColor = redBtn.innerHTML;
+    brushColor = "red";
 }
 function blueColor() {
-    brushColor = blueBtn.innerHTML;
+    brushColor = "blue";
 }
 function greenColor() {
-    brushColor = greenBtn.innerHTML;
+    brushColor = "green";
 }
 function drawClick() {
-    let isDrawing = false;
-
-    canvas.addEventListener("mousedown", function () {
-        isDrawing = true;
+    isClicked = false;
+    canvas.addEventListener('click', function (event) {
+        isClicked = true;
     });
-
-    canvas.addEventListener("mouseup", function () {
-        isDrawing = false;
-        context.beginPath();
-    });
-
     canvas.addEventListener("mousemove", function (event) {
-        if (!isDrawing) return;
+        if (isClicked = true) {
+            console.log(event);
+            context.beginPath();
+            context.moveTo(event.offsetX, event.offsetY);
+            context.lineTo(event.offsetX+1, event.offsetY+1);
 
+            if (brushColor == "red") {
+                context.fillStyle = "red";
+                context.fill();
+                context.closePath();
+            }
+
+            if (brushColor == "blue") {
+                context.fillStyle = "blue";
+                context.fill();
+                context.closePath();
+            }
+
+            if (brushColor == "green") {
+                context.fillStyle = "green";
+                context.fill();
+                context.closePath();
+            }
+        }
+    })
+}*/
+
+
+
+
+//ANIMATION OF A BALL
+/*const canvas = document.querySelector("#canvas");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+canvas.style.background = "lightblue";
+
+const context = canvas.getContext("2d");
+
+let isClicked = false;
+addEventListener('click', function (event) {
+    isClicked = true;
+});
+
+class circle {
+    factorx = 1;
+    factory = 1;
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+    draw() {
+        if(isClicked==true){
         context.beginPath();
-        context.moveTo(event.offsetX, event.offsetY);
-        context.lineTo(event.offsetX+1, event.offsetY+1);
-        context.lineWidth = 5;
-        context.fillStyle = brushColor; 
-        context.stroke();
-        context.closePath();
-    });
+        context.fillStyle = this.color;
+        context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        context.fill();
+        context.closePath;
+    }
+    }
+
+    update() {
+        this.x += this.factorx;
+        this.y += this.factory;
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        this.draw();
+
+        if ((this.y + this.radius) >= canvas.height) {
+            this.factory = -1;
+        }
+
+        if ((this.y - this.radius) <= 0) {
+            this.factory = 1;
+        }
+
+        if ((this.x + this.radius) >= canvas.width) {
+            this.factorx = -1;
+        }
+
+        if ((this.x - this.radius) <= 0) {
+            this.factorx = 1;
+        }
+    }
 }
+
+function updateAnimation() {
+    requestAnimationFrame(updateAnimation);
+    c.update();
+}
+
+let c = new circle(100, 100, 50, "red");
+c.draw();
+c.update();
+updateAnimation();*/
+
+
+
+//DRAWING BALL ANIMATION ON CLICKING AND WHERE CLICKED
+/*const canvas = document.querySelector("#canvas");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+canvas.style.background = "lightblue";
+
+const context = canvas.getContext("2d");
+
+let isClicked = false;
+addEventListener('click', function (event) {
+    isClicked = true;
+});
+
+class circle {
+    factorx = 1;
+    factory = 1;
+    counter = 0;
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+    draw() {
+        if (isClicked == true) {
+            context.beginPath();
+            context.fillStyle = this.color;
+            //context.strokeStyle = "lightblue"
+            context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+            context.font = "25px Arial";
+            context.fillText(this.counter, this.x, this.y);
+            context.textAlign = "center";
+            context.stroke();
+            context.closePath();
+        }
+    }
+
+    update() {
+        this.x += this.factorx;
+        this.y += this.factory;
+        this.draw();
+
+        if ((this.y + this.radius) >= canvas.height) {
+            this.factory = -1;
+            this.counter++;
+        }
+
+        if ((this.y - this.radius) <= 0) {
+            this.factory = 1;
+            this.counter++;
+        }
+
+        if ((this.x + this.radius) >= canvas.width) {
+            this.factorx = -1;
+            this.counter++;
+        }
+
+        if ((this.x - this.radius) <= 0) {
+            this.factorx = 1;
+            this.counter++;
+        }
+    }
+}
+
+function getData(event) {
+    a = event.x;
+    b = event.y;
+}
+
+function updateAnimation(event) {
+    requestAnimationFrame(updateAnimation);
+    this.x=event.x;
+    this.y=event.y;
+    c.update();
+}
+
+let c = new circle(60, 60, 50, "red");
+//c.draw();
+//c.update();
+//updateAnimation();
+addEventListener("click",updateAnimation)
+
+
+let circles = [];
+for (i = 1; i <= 10; i++) {
+    let c = new circle(Math.random() * 1000, Math.random() * 1000, Math.random() * 100, "green");
+    c.draw();
+    circles.push(c);
+}
+function updateAnimation() {
+    requestAnimationFrame(updateAnimation);
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    for (i = 0; i < circles.length; i++) {
+        circles[i].update();
+    }
+    //c.update();
+}
+
+//let c = new circle(60, 60, 50, "red");
+//c.draw();
+//c.update();
+updateAnimation();
+*/
+
+
+
+//COLLISION OF CIRCLES
+const canvas = document.querySelector("#canvas");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+canvas.style.background = "aqua";
+
+const context = canvas.getContext("2d");
+
+/*const img = new Image();
+img.src = "/images/wd.png";
+img.onload = function () {
+    context.drawImage(img, 100, canvas.height - img.height);
+}*/
+
+class circle {
+    factorx = 1;
+    factory = 1;
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+    draw() {
+        context.beginPath();
+        context.strokeStyle = this.color;
+        context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        context.stroke();
+        context.closePath;
+    }
+    update() {
+        this.x += this.factorx;
+        this.y += this.factory;
+        this.draw();
+
+        if ((this.y + this.radius) >= canvas.height) {
+            this.factory = -1;
+            this.counter++;
+        }
+
+        if ((this.y - this.radius) <= 0) {
+            this.factory = 1;
+            this.counter++;
+        }
+
+        if ((this.x + this.radius) >= canvas.width) {
+            this.factorx = -1;
+            this.counter++;
+        }
+
+        if ((this.x - this.radius) <= 0) {
+            this.factorx = 1;
+            this.counter++;
+        }
+    }
+}
+
+let c1 = new circle(200, 200, 40, "red");
+c1.draw();
+
+addEventListener("click", function (e) {
+    if (getDistance(e.x, e.y, c1.x, c1.y) <= 40) {
+        c1.color = "green";
+        c1.draw();
+    }
+})
+
+function getDistance(x1, y1, x2, y2) {
+    let distance = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    return distance;
+}
+
+
+let c2 = new circle(600, 500, 100, "blue");
+c2.draw();
+
+function updateAnimation() {
+    requestAnimationFrame(updateAnimation);
+}
+
+updateAnimation();
